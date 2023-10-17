fd4fe2eb3fa91a017afdd0c077de4d4839b1a878
DabAZ <dieboldhan123@gmail.com>
2023-10-17 13:44:12 +0800

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,19 +3,19 @@ import { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://acme.com',
+      url: 'https://dabaz.me',
       lastModified: new Date(),
       changeFrequency: 'yearly',
       priority: 1,
     },
     {
-      url: 'https://acme.com/about',
+      url: 'https://dabaz.me/about',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
     },
     {
-      url: 'https://acme.com/blog',
+      url: 'https://dabaz.me/blog',
       lastModified: new Date(),
       changeFrequency: 'weekly',
       priority: 0.5,
