ff97f72d5470a7589a02e4d62f05ac095e8b234a
DabAZ <dieboldhan123@gmail.com>
2023-10-31 04:02:39 +0800

    Remove the clock

--- components/sections/Footer.tsx
+++ components/sections/Footer.tsx
@@ -1,11 +1,8 @@
-import Clock from "./Clock";
-
 var today = new Date();
 
 export default function Footer() {
   return (
-    <footer className="max-w-4xl mx-auto w-full flex flex-col items-center justify-center py-8 mt-8 border-t border-white/20">
-      <Clock />
+    <footer className="max-w-4xl mx-auto w-full flex flex-col items-center justify-center mt-8 border-t border-white/20">
       <div className="text-neutral-600 dark:text-neutral-400 flex items-center flex-none">
         &copy; {today.getFullYear()} DabAZ
       </div>
