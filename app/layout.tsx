524b50e8f9a5f6ce83376ed168621ac7f7fe821f
DabAZ <dieboldhan123@gmail.com>
2023-10-31 03:18:31 +0800

    Update layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -72,7 +72,7 @@ export default function RootLayout({
     <html lang="en" className='text-black bg-white dark:text-white dark:bg-[#111010]'>
       <body className='antialiased max-w-4xl mb-40 flex flex-col md:flex-row mx-4 mt-8 md:mt-20 lg:mt-32 lg:mx-auto'>
         <Navbar />
-        <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
+        <main className="flex-auto min-w-0 flex flex-col px-2 md:px-0">
           {children}
         </main>
       </body>
